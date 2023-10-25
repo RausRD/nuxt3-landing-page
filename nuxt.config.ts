36dec7abb8770f0ab17729087d9fe29658d1aef4
Ruslan Dunets <dunecruslan2@gmail.com>
2023-10-25 18:42:15 +0300

    refactor nuxt.config.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Landing Page',
+      meta: [{ name: 'description', content: 'Landing Page Nuxt 3' }],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap',
+        },
+      ],
+    },
+  },
+  css: ['@/assets/main.css'],
+});
