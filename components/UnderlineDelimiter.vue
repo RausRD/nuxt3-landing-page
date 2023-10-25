8b6fd63328b34d08b2618919422377c3653c2f4a
Ruslan Dunets <dunecruslan2@gmail.com>
2023-10-25 18:36:49 +0300

    added UnderlineDelimeter

---
+++ components/UnderlineDelimiter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="underline-delimiter">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="46"
+      height="4"
+      viewBox="0 0 46 4"
+      fill="none"
+      class="delimiter"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M46 3.96493H0V0.965332H46V3.96493Z"
+        fill="#FF2D55"
+      />
+    </svg>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.underline-delimiter {
+  position: relative;
+  width: 46px;
+  height: 4px;
+}
+.underline-delimiter .delimiter {
+  position: absolute;
+}
+</style>
